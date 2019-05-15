7a0f6c42d1e0f25610c3ec405339e1c9e26f2b9a
Sergey Eromchenko <40355944+flunk72@users.noreply.github.com>
2019-05-15 23:31:57 +0500

    доделал тестовое

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('.needs-validation');
+
+form.addEventListener('submit', function (event) {
+    if(form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+});
